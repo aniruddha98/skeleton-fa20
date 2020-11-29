2220169236a73f32297d682489b559dd88e6b422
Aniruddha Vangala <aniruddha.vangala@berkeley.edu>
2020-11-29 15:19:21 -0800

    done 2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -80,8 +80,12 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        IntList p=A;
+        while(p.rest!=null){
+            p=p.rest;
+        }
+        p.rest=B;
+        return A;
     }
 
     /**
@@ -89,8 +93,14 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A == null) {
+
+            if(B==null){
+                return null;
+            }
+            return new IntList(B.first,catenate(A,B.rest));
+        }
+        return new IntList(A.first, catenate(A.rest,B));
     }
 
 
