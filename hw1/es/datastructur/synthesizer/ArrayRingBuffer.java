4cab45b2a508bf5a68908974869c479211a1e126
Aniruddha Vangala <aniruddha.vangala@berkeley.edu>
2020-11-29 17:01:00 -0800

    done hw1

--- hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
@@ -1,19 +1,74 @@
 package es.datastructur.synthesizer;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 //TODO: Make sure to that this class and all of its methods are public
 //TODO: Make sure to add the override tag for all overridden methods
 //TODO: Make sure to make this class implement BoundedQueue<T>
 
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> implements BoundedQueue<T> {
     /* Index for the next dequeue or peek. */
     private int first;
     /* Index for the next enqueue. */
     private int last;
     /* Variable for the fillCount. */
+
     private int fillCount;
     /* Array for storing the buffer data. */
     private T[] rb;
+
+
+    /**Make ArrayRingBuffer object iterable and return an iterator*/
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayRingBufferIterator(first, fillCount, rb);
+    }
+
+    /**private class which implements the Iterator interface and create an Iterator object*/
+    private class ArrayRingBufferIterator implements Iterator<T> {
+        int fillCount;
+        int index;
+        T[] rb;
+
+        public ArrayRingBufferIterator(int first, int fillCount, T[] rb) {
+            this.fillCount = fillCount;
+            this.index = first;
+            this.rb = rb;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (fillCount == 0) {
+                return false;
+            }
+            fillCount -= 1;
+            return true;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T temp = rb[index];
+            rb[index] = null;
+            index++;
+            fillCount--;
+            return temp;
+        }
+    }
+
+    /**return true only if the other object is in an ArrayRingBuffer with the exact same values*/
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof ArrayRingBuffer && this.equals(o)) {
+            return true;
+        }
+        return false;
+    }
+
+
 
     /**
      * Create a new ArrayRingBuffer with the given capacity.
@@ -21,17 +76,30 @@ public class ArrayRingBuffer<T>  {
     public ArrayRingBuffer(int capacity) {
         // TODO: Create new array with capacity elements.
         //       first, last, and fillCount should all be set to 0.
+        rb = (T[]) new Object[capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
     }
 
     /**
      * Adds x to the end of the ring buffer. If there is no room, then
      * throw new RuntimeException("Ring buffer overflow").
      */
+    @Override
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update
         //       last. Don't worry about throwing the RuntimeException until you
         //       get to task 4.
-        return;
+        if (BoundedQueue.super.isFull()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        rb[last] = x;
+        last++;
+        if (last == rb.length) {
+            last = 0;
+        }
+        fillCount++;
     }
 
     /**
@@ -42,21 +110,47 @@ public class ArrayRingBuffer<T>  {
         // TODO: Dequeue the first item. Don't forget to decrease fillCount and
         //       update first. Don't worry about throwing the RuntimeException until you
         //       get to task 4.
-        return null;
+        if (BoundedQueue.super.isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T temp = rb[first];
+        rb[first] = null;
+        first++;
+        if (first == rb.length) {
+            first = 0;
+        }
+        fillCount--;
+        return temp;
     }
 
     /**
      * Return oldest item, but don't remove it. If the buffer is empty, then
      * throw new RuntimeException("Ring buffer underflow").
      */
+    @Override
     public T peek() {
         // TODO: Return the first item. None of your instance variables should
         //       change. Don't worry about throwing the RuntimeException until you
         //       get to task 4.
-        return null;
+        if (BoundedQueue.super.isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        return rb[first];
     }
 
     // TODO: When you get to part 4, implement the needed code to support
     //       iteration and equals.
+
+
+    @Override
+    public int fillCount() {
+        return fillCount;
+    }
+
+    @Override
+    public int capacity() {
+        return rb.length;
+    }
 }
     // TODO: Remove all comments that say TODO when you're done.
+
---
+++ hw1/es/datastructur/synthesizer/BoundedQueue.java
@@ -0,0 +1,44 @@
+package es.datastructur.synthesizer;
+import java.util.Iterator;
+
+/**an interface which declares all the methods
+ * that must be implemented nu amu class
+ * that implements BoundedQueue.
+ * @author ling*/
+public interface BoundedQueue<T> extends Iterable<T> {
+
+    /**return size of the buffer.*/
+    int capacity();
+
+    /**return number of items currently in the buffer.*/
+    int fillCount();
+
+    /**add item x to the end.*/
+    void enqueue(T x);
+
+    /**delete and return item from the front.*/
+    T dequeue();
+
+    /**return (but do not delete) item from the front.*/
+    T peek();
+
+    /**is the buffer empty (fillCount equals zero)?*/
+    default boolean isEmpty(){
+        if (fillCount() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**is the buffer full (fillCount is same as capacity)?*/
+    default boolean isFull() {
+        if (fillCount() == capacity()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**Implement the iterator method to make BoundedQueue object iterable.*/
+    @Override
+    Iterator<T> iterator();
+}
