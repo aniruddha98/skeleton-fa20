86c83c4973223cd02464637693f946937362c819
Aniruddha Vangala <aniruddha.vangala@berkeley.edu>
2020-11-29 17:14:00 -0800

    done with 6

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,11 +1,8 @@
-import javax.swing.*;
+import java.util.ArrayList;
 
 public class UnionFind {
+    int[] parent;
 
-    /**Implement the Weighted Quick Union Algorithm.
-     * We need to connect the relative smaller tree to the bigger one.
-     * To achieve this, we need to add an array in it to store the vertices numbers.*/
-    private int[] parent;
     private int[] id;
     private int[] size;
     private int count;
@@ -24,32 +21,27 @@ public class UnionFind {
         count = n;
     }
 
-    /* Throws an exception if v1 is not a valid index. */
-    private void validate(int vertex) {
-
-        if (vertex < 0 || vertex>count){
-            throw new IllegalArgumentException("Vertex " + vertex + " is not a valid index");
+    /* Throws an exception if v1 is not a valid vertex. */
+    private void validate(int v1) {
+        if (v1 < 0 || v1>count){
+            throw new IllegalArgumentException("Vertex " + v1 + " is not a valid index");
         }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-
-        validate(v1);
-        return -parent(find(v1));
+        int root = find(v1);
+        return -1 * parent[root];
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-
-        validate(v1);
         return parent[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
-    public boolean connected(int v1, int v2) {
-
+    public boolean isConnected(int v1, int v2) {
         validate(v1);
         validate(v2);
         return find(v1) == find(v2);
@@ -57,18 +49,17 @@ public class UnionFind {
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid
        elements, and a union-by-size heuristic is used. If the sizes of the sets
-       are equal, tie break by connecting v1's root to v2's root. Unioning a
+       are equal, tie break by connecting v1's root to v2's root. Connecting a
        vertex with itself or vertices that are already connected should not
        change the sets but may alter the internal structure of the data. */
-    public void union(int v1, int v2) {
-
+    public void connect(int v1, int v2) {
         validate(v1);
         validate(v2);
         int v1Root = find(v1);
         int v2Root = find(v2);
         int v1Size = sizeOf(v1);
         int v2Size = sizeOf(v2);
-        if (!connected(v1,v2)) {
+        if (!isConnected(v1,v2)) {
             if (v1Size >= v2Size) {
                 parent[v2Root] = v1Root;
                 parent[v1Root] = -(v1Size + v2Size);
@@ -80,15 +71,14 @@ public class UnionFind {
 
     }
 
-    /* Returns the root of the set V belongs to. Path-compression is employed
+    /* Returns the root of the set v1 belongs to. Path-compression is employed
        allowing for fast search-time. */
-    public int find(int vertex) {
-
-        validate(vertex);
-        while (vertex != parent[vertex] && parent[vertex] >= 0) {
-            vertex = parent[vertex];
+    public int find(int v1) {
+        validate(v1);
+        while (v1 != parent[v1] && parent[v1] >= 0) {
+            v1 = parent[v1];
         }
-        return vertex;
+        return v1;
     }
 
 }
